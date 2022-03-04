83a8e31302ad3826f43d92ea06aa1ba5403bfaf1
eduardocardona93 <eduardoacm93@gmail.com>
2022-03-04 00:13:01 -0500

    Login Screen mockup

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from './screens/Register';
+const Stack = createNativeStackNavigator();
 
+import Login from './screens/Login'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" options={{headerShown: false}} component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>  );
 }
 
 const styles = StyleSheet.create({
