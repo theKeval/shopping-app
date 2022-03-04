5c0e139b3a93e135c0801bfa71f9eceb22439164
eduardocardona93 <eduardoacm93@gmail.com>
2022-03-04 15:48:24 -0500

    login view finished

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text } from 'react-native'
 import React, { useEffect,useState } from 'react';
 
-import { Button, KeyboardAvoidingView, TextInput, TouchableOpacity, View } from 'react-native'
+import { Button, KeyboardAvoidingView, TextInput, TouchableOpacity, View,Image } from 'react-native'
+import { Ionicons,FontAwesome5,AntDesign   } from '@expo/vector-icons';
 
+import Logo from '../assets/mango_letter.png';
+import MangoStyles   from '../styles'
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 const Login = () => {
+  const {height} = useWindowDimensions();
     const [email, emailSetText] = useState('');
     const [password, passwordSetText] = useState('');
     const onChangeEmail = textValue => emailSetText(textValue);
@@ -12,57 +17,90 @@ const Login = () => {
     const onPressRegister = () => {}
   return (
 
-    <KeyboardAvoidingView
-    style={styles.container} behavior ="padding">
-    <View style={styles.inputContainer}> 
-        <TextInput placeholder="Email" value={email} onChangeText={onChangeEmail} style={styles.input}/>
-        <TextInput placeholder="Password" value={password} onChangeText={onChangePass } style={styles.input}/>
-    </View>
-    <View style={styles.buttonContainer}> 
+    <View style={styles.container} >
+      <Image source={Logo}  style={[styles.logo, {height: height*0.3}]} resizeMode="contain" />
+      <View style={styles.inputContainer}> 
+          
+          <View style={styles.inputIcon}>
+            <FontAwesome5 name="user-circle" size={20} color="black" />
+          </View>
+          <TextInput placeholder="Email" value={email} onChangeText={onChangeEmail} style={styles.input}/>
 
-    <TouchableOpacity
-          onPress={onPressLogin}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={onPressRegister}
-          style={[styles.button, styles.buttonOutline]}
-        >
-          <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-    </View>
-</KeyboardAvoidingView>
+      </View>
+      <View style={styles.inputContainer}> 
+          <View style={styles.inputIcon}>
+            <FontAwesome5 name="key" size={20} color="black" />
+          </View>
+          <TextInput placeholder="Password" value={password} onChangeText={onChangePass } style={styles.input}/>
+      </View>
+      <View style={styles.buttonContainer}> 
+
+      <TouchableOpacity
+            onPress={onPressLogin}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={onPressRegister}
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>Register</Text>
+          </TouchableOpacity>
+      </View>
+</View>
   )
 }
 
 
 
 const styles = StyleSheet.create({
-    container: {
+      container: {
+        padding: 10,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: MangoStyles.mangoPaleOrange,
+      },
+      logo:{
+        width:'90%',
+        maxWidth: 500,
+        maxHeight:150,
+        marginBottom: 50,
       },
       inputContainer: {
-        width: '80%'
-      },
-      input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-      },
-      buttonContainer: {
-        width: '60%',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 40,
+        marginBottom: 10,
+        flexDirection:'row',
+        width: '90%'
+      },
+      inputIcon: {
+        borderColor: MangoStyles.mangoOrangeYellow,
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 13,
+        marginRight: 10,
+        backgroundColor: 'white',
+      },
+      input: {
+        width: '80%',
+        backgroundColor: 'white',
+        borderColor: MangoStyles.mangoOrangeYellow,
+        borderWidth: 2,
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        
+      },
+      buttonContainer: {
+        width: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
       },
       button: {
-        backgroundColor: '#0782F9',
+        backgroundColor: MangoStyles.mangoOrangeYellow,
         width: '100%',
         padding: 15,
         borderRadius: 10,
@@ -71,7 +109,7 @@ const styles = StyleSheet.create({
       buttonOutline: {
         backgroundColor: 'white',
         marginTop: 5,
-        borderColor: '#0782F9',
+        borderColor: MangoStyles.mangoOrangeYellow,
         borderWidth: 2,
       },
       buttonText: {
@@ -80,7 +118,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
       },
       buttonOutlineText: {
-        color: '#0782F9',
+        color: MangoStyles.mangoOrangeYellow,
         fontWeight: '700',
         fontSize: 16,
       },
---
+++ styles.js
@@ -0,0 +1,8 @@
+
+export default MangoStyles = {
+    mangoOrangeYellow: '#FFC44E' ,
+    mangoPaleOrange: '#FFFAF1' ,
+    mangoPositiveAction: '#8DB228' ,
+    mangoNegativeAction: '#FF4E4E' ,
+    mangoGray : '#00000080' ,
+}
