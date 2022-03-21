0784e7cc09b193da99a36ac98cbe04440168c4ec
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-03-21 12:50:26 -0400

    users info screen

--- screens/UserInfoScreen.js
+++ screens/UserInfoScreen.js
@@ -18,13 +18,57 @@ const UserInfoScreen = ({navigation, route}) => {
         })
         return unsubscribe;
     }, [navigation]);
+
   return (
-    <View>
-      <Text>UserInfoScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.label}>User</Text>
+        <Text style={styles.content}>{userInfo? userInfo.name:""}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Email</Text>
+        <Text style={styles.content}>{userInfo? userInfo.email:""}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Address</Text>
+        <Text style={styles.content}>{userInfo? userInfo.address:""}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Phone</Text>
+        <Text style={styles.content}>{userInfo? userInfo.phoneNumber:""}</Text>
+      </View>
     </View>
   )
 }
 
 export default UserInfoScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 18,
+    flex: 1,
+    backgroundColor: MangoStyles.mangoPaleOrange,
+    paddingHorizontal: 10,
+    paddingTop:20,
+  },
+  
+  label: {
+    fontSize: 18,
+    fontWeight: '700',
+    width: '50%',
+  },
+
+  content: {
+    fontSize: 16,
+    width: '50%',
+    textAlignVertical: 'center',
+    textAlign: 'right'
+  },
+
+  row:{
+    paddingVertical: 10,
+    flexDirection:'row',
+    justifyContent: 'space-between'
+  },
+
+})
